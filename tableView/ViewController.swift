cb5824b3abcb6feb6bf609b5084e8939b6d035d4
1lugesya <sariaysegulll@gmail.com>
2022-07-14 19:39:53 +0300

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Kodluyoruz Dernegi on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
